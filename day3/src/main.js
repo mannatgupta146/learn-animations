0681d68fdc502b5337076c35b34f359d33206d74
Mannat Gupta <mannatgupta146@gmail.com>
2026-06-28 01:25:01 +0530

    stagger animations for heading letters

--- day3/src/main.js
+++ day3/src/main.js
@@ -1,10 +1,26 @@
 import { gsap } from 'gsap'
 import './style.css'
 
-gsap.to('.box', {
-    x: 1500,
-    duration: 1.5,
-    delay: 0.6,
-    ease: "power1.out",
-    repeat: -1,
+// gsap.to('.box', {
+//     x: 1300,
+//     duration: 1.5,
+//     delay: 0.6,
+//     ease: "power1.out",
+//     repeat: -1,
+//     stagger: {
+//         each: 0.2,
+//         from: "random",
+//     },
+// })
+
+gsap.from("h1 span", {
+    yPercent: 100,
+    duration: 0.6,
+    opacity: 0,
+    delay: 0.2,
+    ease: "expo.out",
+    stagger: {
+        each: 0.1,
+        from: "start"
+    },
 })
