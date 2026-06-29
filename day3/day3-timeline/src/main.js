d44397ed4e42fd7150ae7c3382e73cebfb6bd178
Mannat Gupta <mannatgupta146@gmail.com>
2026-06-29 17:44:45 +0530

    parameters

--- day3/day3-timeline/src/main.js
+++ day3/day3-timeline/src/main.js
@@ -12,7 +12,7 @@ tl.to("#box1", {
     x: 1200,
     duration: 1,
     ease: "power4.out",
-}).to("#box3", {
+}, "man").to("#box3", {
     x: 1200,
     duration: 1,
     ease: "power4.out",
@@ -20,4 +20,4 @@ tl.to("#box1", {
     x: 1200,
     duration: 1,
     ease: "power4.out",
-})
+}, "man")
