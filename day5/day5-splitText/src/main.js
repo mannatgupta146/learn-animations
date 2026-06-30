f249cddfe5a14343ddb56870d020925ffa3b7ca8
Mannat Gupta <mannatgupta146@gmail.com>
2026-07-01 02:05:54 +0530

    setup initial HTML structure for day5-splitText project

---
+++ day5/day5-splitText/src/main.js
@@ -0,0 +1,7 @@
+import { gsap } from "gsap";
+import "./style.css";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(ScrollTrigger,SplitText);
