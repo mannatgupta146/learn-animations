c23079fc485036bda99fc2c0d8c1bdc908f1b790
Mannat Gupta <mannatgupta146@gmail.com>
2026-07-01 00:56:53 +0530

    scrolltrigger box animation on scroll

--- day4/day4-scrollTrigger/src/main.js
+++ day4/day4-scrollTrigger/src/main.js
@@ -1,5 +1,16 @@
 import { gsap } from "gsap";
-    
+import "./style.css";
+
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".box", {
+    x: 1200,
+    duration: 1.5,
+    ease: "power1.inOut",
+    scrollTrigger: {
+        trigger: ".box",
+        start: "top 30%",
+    }
+})
