5d365fc34d3f13201c003865294138cb9250d732
Mannat Gupta <mannatgupta146@gmail.com>
2026-07-01 18:33:09 +0530

    flip image to imageShow on click with animation

--- day5/day5-flip/src/main.js
+++ day5/day5-flip/src/main.js
@@ -8,3 +8,16 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
 
 gsap.registerPlugin(Draggable,Flip,InertiaPlugin,ScrollTrigger,SplitText);
+
+const flipImage = document.querySelector(".flipImage");
+const imageShow = document.querySelector(".imageShow");
+
+flipImage.addEventListener("click", () => {
+    const state = Flip.getState(flipImage);
+    imageShow.appendChild(flipImage);
+
+    Flip.from(state, {
+        duration: 0.8,
+        ease: "power1.inOut",
+    })
+})
