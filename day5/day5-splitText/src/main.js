c67a77a2c600f0f6a55bf7f13513b893c02d14b6
Mannat Gupta <mannatgupta146@gmail.com>
2026-07-01 14:46:13 +0530

    split text animation for title on scroll

--- day5/day5-splitText/src/main.js
+++ day5/day5-splitText/src/main.js
@@ -5,3 +5,16 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
 
 gsap.registerPlugin(ScrollTrigger,SplitText);
+
+const split = new SplitText(".title h1", { type: "chars, words, lines" });
+
+gsap.from(split.lines, {
+    yPercent: 100,
+    opacity: 0,
+    duration: 1,
+    ease: "expo.out",
+    stagger: {
+        each: 0.5,
+        from: "start"
+    }
+})
