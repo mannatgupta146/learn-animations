97b99df53f4d9aeb4dcaebf4d9195d5cf34fcd0f
Mannat Gupta <mannatgupta146@gmail.com>
2026-07-02 17:19:05 +0530

    motion in react

--- DAY6/day6-motion/src/App.jsx
+++ DAY6/day6-motion/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { motion } from 'motion/react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <motion.div 
+        initial={{ x: 0, opacity: 0}}
+        transition={{duration: 0.8, delay: 0.6, ease: "easeInOut", damping: 0.5}} 
+        animate={{ x: 800, opacity: 1 }} 
+        className="box">
+      </motion.div>
+    </div>
   )
 }
 
